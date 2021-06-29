bc3adc0f8930ecb32e616eede6861b8fabc551a6
ankit-karwasra <akarwasra.10@gmail.com>
2021-06-30 01:41:06 +0530

    refresh done

---
+++ index.js
@@ -0,0 +1,78 @@
+// Parent element to store cards
+const taskContainer = document.querySelector(".task__container");
+console.log(taskContainer);
+
+// gLobal Store
+const globalStore = [];
+
+
+const newCard = ({
+    id,
+    imageUrl,
+    taskTitle,
+    taskDescription,
+    taskType
+}) => ` <div class="col-md-6 col-lg-4" id=${id}>
+     <div class="card ">
+         <div class="card-header d-flex justify-content-end gap-2">
+             <button type="button" class="btn btn-outline-success"><i
+                     class="fas fa-pencil-alt"></i></button>
+             <button type="button" class="btn btn-outline-danger"><i
+                     class="fas fa-trash-alt"></i></button>
+
+         </div>
+         <img src=${imageUrl}
+             class="rounded-end" alt="...">
+         <div class="card-body">
+             <h5 class="card-title">${taskTitle}</h5>
+             <p class="card-text">
+             ${taskDescription}
+             </p>
+             <span class="badge bg-primary">${taskType}</span>
+         </div>
+         <div class="card-footer text-muted">
+             <button type="button" class="btn btn-outline-primary float-end">
+                 Open Task
+             </button>
+         </div>
+     </div>
+ </div>`
+
+const loadInitiatTaskCards = () => {
+    // access locastorage
+    const getInitialData = localStorage.getItem("tasky");
+    if (!getInitialData) return;
+
+    //convert stringified-object to object
+    const { cards } = JSON.parse(getInitialData);
+
+    //map around the array to generate HTML card and inject it to DOM
+    cards.map((cardObject) => {
+        const createNewCard = newCard(cardObject);
+        taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+        globalStore.push(cardObject);
+    });
+
+};
+
+
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`, // unique number for card id
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value,
+    };
+
+    // HTML code
+    const createNewCard = newCard(taskData);
+
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+    globalStore.push(taskData);
+
+    // add to localstorage
+    localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+
+
+};
